9934b08712412d4798498a8aeb2c498bfa9b1194
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-06-11 14:48:46 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include < iostream>
+#include < vector>
+#include < string>
+
+using namespace std;
+
+class GroceryStore {       // The class
+
+};
+
+
+int main()
+{
+	GroceryStore();//Call class
+	return 0;
+}
