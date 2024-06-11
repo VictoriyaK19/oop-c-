1c5afbde03bfa4b31f7f86b25f83d9c0f7f0be63
VictoriyaK19 <kalinovavictoriya@gmail.com>
2024-06-11 16:21:22 +0300

    constructor

--- main.cpp
+++ main.cpp
@@ -5,12 +5,17 @@
 using namespace std;
 
 class GroceryStore {       // The class
-
+public:
+	string item1;
+	GroceryStore(string item, int price, int wallet) {
+		item1 = item;
+	}
 };
 
 
 int main()
 {
-	GroceryStore();//Call class
+	GroceryStore Bob("milk", 15, 20);  //Call class
+	cout << Bob.item1 << endl;
 	return 0;
 }
